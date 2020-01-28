8114090501a48c297d49b751976534563fb1bc62
hackpascal <hackpascal@gmail.com>
2020-01-28 22:27:16 +0800

    add upstream patches & gcc build fix

--- package/gcc/final.mk
+++ package/gcc/final.mk
@@ -4,19 +4,15 @@ CONFIGURE_ARGS += \
 	--disable-libstdcxx-pch \
 	--enable-libgomp \
 	--enable-libssp \
-	--enable-libmudflap \
-	--enable-libatomic \
-	--enable-libvtv \
-	--enable-libquadmath \
 	--enable-lto \
 	--enable-largefile \
 	--enable-shared \
 	--enable-static \
 	--enable-nls \
 	--enable-tls \
+	--enable-threads=$(THREAD_MODEL) \
 	--enable-checking=release \
 	--enable-libstdcxx-debug \
 	--enable-libstdcxx-time=yes \
-	--enable-languages=c,c++,lto \
-	--enable-threads=$(THREAD_MODEL)
+	--enable-languages=c,c++,lto
 
